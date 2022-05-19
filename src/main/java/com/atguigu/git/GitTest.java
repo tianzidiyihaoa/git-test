27f87bd66e00505220014d3ef7c58739c080c59f
711216MZS <18745199568>
2022-05-19 12:32:49 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Author 孟昭岁
+ * @Create 2022/5/19 12:29
+ * @Version
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
